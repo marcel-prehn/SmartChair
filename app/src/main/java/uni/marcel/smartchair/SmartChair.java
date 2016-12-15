3474cb887608d577ba11cb2630828bbaf18ab415
mz47 <mpnw47@gmail.com>
2016-12-15 13:49:02 +0100

    Filled Sensor Class

--- app/src/main/java/uni/marcel/smartchair/SmartChair.java
+++ app/src/main/java/uni/marcel/smartchair/SmartChair.java
@@ -2,4 +2,44 @@ package uni.marcel.smartchair;
 
 public class SmartChair {
 
+    private Sensor[] sensors;
+
+    public SmartChair() {
+
+    }
+
+    public Sensor[] getSensors() {
+        return this.sensors;
+    }
+
+    public void setSensors(Sensor[] sensors) {
+        if(sensors != null) {
+            this.sensors = sensors;
+        }
+    }
+
+    public void addSensor(Sensor s) {
+        if(s != null) {
+            Sensor[] temp = new Sensor[sensors.length + 1];
+            for(int i = 0; i < sensors.length; i++) {
+                temp[i] = sensors[i];
+            }
+            temp[sensors.length] = s;
+            this.sensors = temp;
+        }
+    }
+
+    public Sensor getSensor(int id) {
+        if(id >= 0) {
+            for(Sensor s : sensors) {
+                if(s.getId() == id) {
+                    return s;
+                }
+            }
+            return null;
+        }
+        else {
+            return null;
+        }
+    }
 }
